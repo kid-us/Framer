449480b644af09d677386a9ac35be57cf9c814ff
kid-us <kidushh29@gmail.com>
2024-10-02 16:38:04 +0300

    Simple Rotate animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import "./App.css";
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
+  const [isVisible, setIsVisible] = useState<boolean>(true);
   return (
     <>
       <div className="container mx-auto py-5">
-        <p className="text-xl font-bold">Hello Framer Motion Welcome</p>
+        <div className="grid justify-center items-center h-[100vh] gap-7">
+          <div>
+            <motion.button
+              onClick={() => setIsVisible(!isVisible)}
+              className="bg-blue-800 rounded py-2 mb-2 w-[150px]"
+              layout
+            >
+              Show / Hide
+            </motion.button>
+
+            <AnimatePresence mode="popLayout">
+              {isVisible && (
+                <motion.div
+                  initial={{ rotate: "0deg", scale: 0 }}
+                  animate={{ rotate: "180deg", scale: 1 }}
+                  exit={{ rotate: "0deg", scale: 0 }}
+                  transition={{ duration: 1, ease: "backInOut" }}
+                  className="w-[150px] h-[150px] bg-white"
+                ></motion.div>
+              )}
+            </AnimatePresence>
+          </div>
+        </div>
       </div>
     </>
   );
