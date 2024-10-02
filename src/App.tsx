3ba520106a60ee13e12b3bbba1413d01e994ac85
kid-us <kidushh29@gmail.com>
2024-10-02 15:51:34 +0300

    Dev Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto py-5">
+        <p className="text-xl font-bold">Hello Framer Motion</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
