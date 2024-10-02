f11ba5ba9a2364af83beb9af4c85af62884bff9d
kid-us <kidushh29@gmail.com>
2024-10-02 16:45:30 +0300

    Bounce box

--- src/App.tsx
+++ src/App.tsx
@@ -20,10 +20,18 @@ function App() {
             <AnimatePresence mode="popLayout">
               {isVisible && (
                 <motion.div
-                  initial={{ rotate: "0deg", scale: 0 }}
-                  animate={{ rotate: "180deg", scale: 1 }}
-                  exit={{ rotate: "0deg", scale: 0 }}
-                  transition={{ duration: 1, ease: "backInOut" }}
+                  initial={{ rotate: "0deg", scale: 0, y: 0 }}
+                  animate={{
+                    rotate: "180deg",
+                    scale: 1,
+                    y: [0, 50, -50, -50, 0],
+                  }}
+                  exit={{ rotate: "0deg", scale: 0, y: 0 }}
+                  transition={{
+                    duration: 1,
+                    ease: "backInOut",
+                    times: [0, 0.25, 0.5, 0.85, 1],
+                  }}
                   className="w-[150px] h-[150px] bg-white"
                 ></motion.div>
               )}
